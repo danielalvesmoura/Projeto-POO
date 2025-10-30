d8fd3206965fdee0f3893ce50bb9d34e4b87ec11
danielalvesmoura <danielalvesmoura2006@gmail.com>
2025-10-29 22:10:58 -0300

    feat-botoes

--- PortalX/src/main/java/main/controller/SecaoDashboard.java
+++ PortalX/src/main/java/main/controller/SecaoDashboard.java
@@ -1,9 +1,8 @@
-package main;
+package main.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
-public class Controller {
+public class SecaoDashboard {
 
     @FXML
     private void botaoClicado() {
